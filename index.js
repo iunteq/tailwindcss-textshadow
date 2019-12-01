dd71382f6a45226f54162479603de7dafe3f7e17
iunteq <42003389+iunteq@users.noreply.github.com>
2019-12-01 12:49:07 +0530

    First commit!

---
+++ index.js
@@ -0,0 +1,18 @@
+const _ = require('lodash')
+
+module.exports = function() {
+	return function({ addUtilities, e, theme, variants }) {
+      const utilities = _.fromPairs(
+   	   _.map(theme('textShadow'), (value, modifier) => {
+            const className = modifier === 'default' ? 'text-shadow' : `${e(`text-shadow-${modifier}`)}`
+        	   return [
+               `.${className}`,
+             	{
+             		'text-shadow': value,
+             	},
+        	   ]
+         })
+      )
+      addUtilities(utilities, variants('textShadow'))
+	}
+}
