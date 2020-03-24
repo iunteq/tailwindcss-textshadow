e0784938431ca68e118ed769bf9eb487de0f21c6
roydebangshu <58878367+roydebangshu@users.noreply.github.com>
2020-03-24 16:08:57 +0530

    Function declarartion pattern changed.

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const _ = require('lodash')
 
-module.exports = function() {
-	return function({ addUtilities, e, theme, variants }) {
-      const utilities = _.fromPairs(
-   	   _.map(theme('textShadow'), (value, modifier) => {
-            const className = modifier === 'default' ? 'text-shadow' : `${e(`text-shadow-${modifier}`)}`
-        	   return [
-               `.${className}`,
-             	{
-             		'text-shadow': value,
-             	},
-        	   ]
-         })
-      )
-      addUtilities(utilities, variants('textShadow'))
-	}
+module.exports = function({ addUtilities, e, theme, variants }) {
+  const utilities = _.fromPairs(
+	   _.map(theme('textShadow'), (value, modifier) => {
+        const className = modifier === 'default' ? 'text-shadow' : `${e(`text-shadow-${modifier}`)}`
+    	   return [
+           `.${className}`,
+         	{
+         		'text-shadow': value,
+         	},
+    	   ]
+     })
+  )
+  addUtilities(utilities, variants('textShadow'))
 }
+
